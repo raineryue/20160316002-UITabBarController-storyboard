1f1dcbddd8f694f9d18c73915b6e2c23c4f157eb
raineryue <it_jinlei.yue@163.com>
2016-03-16 16:27:00 +0800

    Initial Commit

---
+++ 20160316002-UITabBarController-storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160316002-UITabBarController-storyboard
+//
+//  Created by Rainer on 16/3/16.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
